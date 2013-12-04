1baac27946d98f7f4238cd229e58950296e43b2e
Jochen Maes <jochen@sejo-it.be>
2013-12-04 12:50:44 +0100

    sets correct theme
    Signed-off-by: Jochen Maes <jochen@sejo-it.be>

--- plugins/git-prompt/git-prompt.plugin.zsh
+++ plugins/git-prompt/git-prompt.plugin.zsh
@@ -54,7 +54,7 @@ function prompt_git_info() {
 }
 
 # Set the prompt.
-#PROMPT='%B%m%~%b$(prompt_git_info) %# '
+PROMPT='%B%m%~%b$(prompt_git_info) %# '
 # for a right prompt:
 #RPROMPT='%b$(prompt_git_info)'
 RPROMPT='$(prompt_git_info)'
